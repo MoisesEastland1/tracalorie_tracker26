b208aa7712dd6eaf8916f17cbfd860148fccab78
MoisesEastland1 <meastland977@gmail.com>
2026-02-04 20:03:19 -0600

    added DCT and render method

--- js/app.js
+++ js/app.js
@@ -4,17 +4,31 @@ class CalorieTracker {
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
+
+    this._displayCaloriesTotal();
   }
 
-  //Public method
+  //Public Methods
   addMeal(meal){
     this._meals.push(meal);
-    this._totalCalories += meal.calores;
+    this._totalCalories += meal.calories;
+    this._render();
   }
 
   addWorkout(workout){
     this._workouts.push(workout);
-    this._totalCalories -= workout.calores;
+    this._totalCalories -= workout.calories;
+    this._render();
+  }
+
+  //Private Methods
+  _displayCaloriesTotal(){
+    const totalCaloriesEl = document.getElementById('calories-total');
+    totalCaloriesEl.innerHTML = this._totalCalories;
+  }
+
+  _render() {
+    this._displayCaloriesTotal();
   }
 }//this is the end of CalorieTracker class
 
@@ -32,5 +46,24 @@ class Workout {
     this.name = name;
     this.calories = calories;
   }
-}//this is the end Workout class
+}//this is the end of Workout class
+
+//init tracker
+const tracker = new CalorieTracker();
+
+const breakfast = new Meal('Breakfast', 400);
+tracker.addMeal(breakfast);
+
+const lunch = new Meal('Lunch', 350);
+tracker.addMeal(lunch);
+
+
+
+const lift = new Workout('Lift Weights', 300);
+tracker.addWorkout(lift);
+
+console.log(tracker._meals);
+console.log(tracker._workouts);
+console.log(tracker._totalCalories);
+
 
